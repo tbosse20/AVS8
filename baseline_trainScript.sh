8f578e7b0689f5f48ec1445dfddaf559e067f322
mputak <mputak98@gmail.com>
2024-05-16 11:04:37 +0200

    sh changed

--- baseline_trainScript.sh
+++ baseline_trainScript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-srun --time=1-00 singularity run --nv /home/student.aau.dk/lk83xy/pytorch-24.01 python baseline_tts_pipeline.py -n "baseline" --train --base
+srun --time=1-00 singularity run --nv /home/student.aau.dk/lk83xy/pytorch-24.01 python baseline_tts_pipeline.py -n "baseline chpnt" --train --base --checkpoint_run "/home/student.aau.dk/lk83xy/avs8/AVS8/runs/run-May-10-2024_06+16PM-c65a463"
