05b24754df261499604cb0e00e67b7a669b69c10
Tonko <tbosse20@student.aau.dk>
2024-05-21 08:27:30 +0200

    plot_funcs to contain both lists

--- plot_funcs.py
+++ plot_funcs.py
@@ -1,42 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-def plot_boxplot(numpy_array, file_name):
+def plot_boxplot(file_name, baseline_array, slimelime_array=None):
+    
+    # Set the style of the visualization
+    alpha = 0.4 # Transparency of the histograms
+    bins = 20 # Number of bins for the histograms
     
     # Generate random data to test
     # numpy_array = np.random.normal(loc=0, scale=1, size=1000)
-
-    # Plot boxplot
-    # plt.boxplot(numpy_array)
+    # numpy_array2 = np.random.normal(loc=0.2, scale=1.2, size=1000)
     
     # Print the number of samples
-    print(f'Cos sim samples: {len(numpy_array)}')
+    print(f'Baseline samples: {len(baseline_array)}')
+    print(baseline_array)
     
-    # Plot the distribution
-    plt.hist(numpy_array, bins=30, color='skyblue', edgecolor='black')
+    # Set the style of the visualization
+    sns.set(style="whitegrid")
+    # Create the density plots
+    plt.figure(figsize=(10, 6))
     
-    plt.xlabel("Cosine Similarity Loss Value")
-    plt.ylabel("Frequency")
+    # Plot Baseline historgam and KDE
+    # sns.histplot(numpy_array, bins=bins, kde=True, color='blue', edgecolor='black', alpha=alpha)
+    sns.kdeplot(baseline_array, color='red', label='Baseline', linewidth=2, fill=True)
+    plt.axvline(np.mean(baseline_array), color='r', linestyle='--')
+    
+    # Plot Slimelime histogram and KDE
+    if slimelime_array is not None:
+        print(f'Slimelime samples: {len(slimelime_array)}')
+        # sns.histplot(numpy_array2, bins=bins, kde=True, color="red", edgecolor='black', alpha=alpha)
+        sns.kdeplot(slimelime_array, color='blue', label='Slimelime', linewidth=2, fill=True)
+        plt.axvline(np.mean(slimelime_array), color='b', linestyle='--')
+
+    # Configure the plot
+    plt.xlabel("Cosine Similarity Loss")
+    plt.ylabel("Denisty")
     # plt.title('Cosine Similarity Boxplot')
-
-    # Format the x-axis and y-axis ticks to be integers and reduce the number of ticks
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(integer=True))
-    plt.gca().yaxis.set_major_locator(plt.MaxNLocator(integer=True))
-
+    plt.legend()
+    
+    # Save the plot
     plt.savefig(f"output/{file_name}.png")
 
 
 if __name__ == "__main__":
     
-    # Add the number of samples to the file name
-    samples = f'_{123}'
-    
     # Define the file names
-    file_name = f"cosSimilarityLoss_baseline_24.4k_steps_baseline{samples}"
+    file_name = f"cosSimilarityLoss_baseline_24.4k_steps_baseline"
+    # Add the number of samples to the file name
+    # file_name += f'_{123}'
     
     # Load the cosine similarity values
     cos_sims_np = np.load(f"output/{file_name}.npy")
+    cos_sims_np_fake = np.load(f"output/{file_name}.npy") * 1.2
 
     # Call the function to plot the boxplot
-    plot_boxplot(cos_sims_np, file_name)
+    plot_boxplot(file_name, cos_sims_np, cos_sims_np_fake)
