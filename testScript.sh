a5a0f52bf57a4884663e043275cf40e96e729987
mputak <mputak98@gmail.com>
2024-05-22 09:04:27 +0200

    working testScript

--- testScript.sh
+++ testScript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/student.aau.dk/lk83xy/avs8/AVS8/tts_pipeline.py --checkpoint_run=/home/student.aau.dk/lk83xy/avs8/AVS8/runs/clmodel_23_8k/ --test
+srun --time=1-00 singularity run --nv /home/student.aau.dk/lk83xy/pytorch-24.01 python tts_pipeline.py --checkpoint_run=/home/student.aau.dk/lk83xy/avs8/AVS8/runs/clmodel_23_8k/ --test
