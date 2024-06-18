a82974e714dcb71093b41a79c396bac5553ccd19
Tonko <tbosse20@student.aau.dk>
2024-06-18 20:33:43 +0200

    yeah sure

--- analysis/compare_average.py
+++ analysis/compare_average.py
@@ -2,41 +2,65 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+metric = [
+    # 'loader_time',
+    'decoder_loss',
+    'postnet_loss', 
+    'similarity_loss',
+    'infonce_loss',
+    'stopnet_loss',
+    'decoder_coarse_loss',
+    'decoder_ddc_loss', 
+    'ga_loss',
+    'decoder_diff_spec_loss', 
+    'postnet_diff_spec_loss',
+    'decoder_ssim_loss', 
+    'postnet_ssim_loss', 
+    'loss',
+    'align_error'
+]
+
 # Data for CLmodel
 clmodel_data = {
-    'Metric': [
-        'decoder_loss', 'postnet_loss', 
-        'similarity_loss', 'infonce_loss', 'stopnet_loss',
-        'decoder_coarse_loss', 'decoder_ddc_loss', 'ga_loss',
-        'decoder_diff_spec_loss', 'postnet_diff_spec_loss',
-        'decoder_ssim_loss', 'postnet_ssim_loss', 'loss',
-        'align_error'
-    ],
+    'Metric': metric,
     'CLmodel': [
-        3.407711613946546, 1.7241535632873026, 
-        1.5607554778735504, 4.221498418994357, 0.5356772717963629,
-        3.155016771720998, 0.0008339224229668403, 0.0034714720227412895,
-        0.1851753995844828, 0.18158624359946737, 0.4859883688591622,
-        0.458419157908513, 4.39831938664284, 0.9919275098941105
+        # 9.916157188633624,  # avg_loader_time
+        1.496860270688539,  # avg_decoder_loss
+        1.049160287558661,  # avg_postnet_loss
+        1.0932102915898683,  # avg_similarity_loss
+        3.0881042891877093,  # avg_infonce_loss
+        0.20904734399239389,  # avg_stopnet_loss
+        1.466936400427391,  # avg_decoder_coarse_loss
+        0.001256453122032648,  # avg_decoder_ddc_loss
+        0.0030530489470011967,  # avg_ga_loss
+        0.18074299745153247,  # avg_decoder_diff_spec_loss
+        0.15799877772460108,  # avg_postnet_diff_spec_loss
+        0.4555105560782546,  # avg_decoder_ssim_loss
+        0.4023916798173266,  # avg_postnet_ssim_loss
+        2.5723555900946478,  # avg_loss
+        0.9899604290863209  # avg_align_error
     ]
 }
 
 # Data for Baseline
 baseline_data = {
-    'Metric': [
-        'decoder_loss', 'postnet_loss', 
-        'similarity_loss', 'infonce_loss', 'stopnet_loss',
-        'decoder_coarse_loss', 'decoder_ddc_loss', 'ga_loss',
-        'decoder_diff_spec_loss', 'postnet_diff_spec_loss',
-        'decoder_ssim_loss', 'postnet_ssim_loss', 'loss',
-        'align_error'
-    ],
+    'Metric': metric,
     'Baseline': [
-        3.471994733612157, 1.7716488218604898, 
-        1.6558103856326636, 4.498039897415094, 0.4623983670916726,
-        3.3480118733681663, 0.0012244461503619504, 0.0033702696479949914,
-        0.17923033063971816, 0.1796362874041972, 0.4767701459773613,
-        0.4499532797232487, 4.487329770026738, 0.9915197661529893
+        # 5.60097294488221,  # avg_loader_time
+        1.7137772330623153,  # avg_decoder_loss
+        0.9246795343510078,  # avg_postnet_loss
+        1.0910505636318317,  # avg_similarity_loss
+        3.390650556885527,  # avg_infonce_loss
+        0.19564336649841188,  # avg_stopnet_loss
+        1.6648003024023932,  # avg_decoder_coarse_loss
+        0.0013432884363751866,  # avg_decoder_ddc_loss
+        0.0030079431318253925,  # avg_ga_loss
+        0.17751935424898865,  # avg_decoder_diff_spec_loss
+        0.15527568359446892,  # avg_postnet_diff_spec_loss
+        0.4474781937509961,  # avg_decoder_ssim_loss
+        0.3828787943677445,  # avg_postnet_ssim_loss
+        2.698046461212412,  # avg_loss
+        0.9902259606564042  # avg_align_error
     ]
 }
 
